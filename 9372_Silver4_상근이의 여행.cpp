ca1e7d8bb53b556880720a2403e61adfae2e7108
strain7626 <78674123+strain7626@users.noreply.github.com>
2025-03-23 21:25:34 +0900

    2025-03-23

---
+++ 9372_Silver4_상근이의 여행.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+    int T; cin >> T;
+    while (T--) {
+        int N, M, a, b; cin >> N >> M;
+        while (M--) cin >> a >> b;
+        cout << N-1 << "\n";
+    }
+}
